1c146809aca599001cdf85ff3ff1e4bdce6b64ba
Jason <jasong@datacom.co.nz>
2018-08-23 18:55:41 +1200

    Post endpoint take 2.

--- app.js
+++ app.js
@@ -17,20 +17,66 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.post('/api/message', (req, res) => {
   var type = req.param('type');
   
-  io.on('connection', (socket) => {
-    var addedUser = false;
-  
-    // when the client emits 'new message', this listens and executes
-    socket.on('new message', (data) => {
-      // we tell the client to execute 'new message'
-      socket.broadcast.emit('new message', {
-        username: "Server",
-        message: type
-      });
-      res.status(200);
-	res.send(type);
-  res.end();
+  var numUsers = 0;
+
+io.on('connection', (socket) => {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: "Server",
+      message: type
     });
+    res.status(200);
+    res.send(type);
+    res.end();
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if ("Server") return;
+
+    // we store the username in the socket session for this client
+    socket.username = "Server";
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: "Server",
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: "Server"
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: "Server"
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: "Server",
+        numUsers: numUsers
+      });
+    }
   });
 });
 
